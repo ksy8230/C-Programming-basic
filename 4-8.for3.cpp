fdb0af1e0674da83204384d19fa8139051e61ecd
ksy <ksy8230@hanmail.net>
2021-03-28 18:14:01 +0900

    선택 제어문 for - 다섯개의 정숫값 받아 총합 구하기

---
+++ 4-8.for3.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+/*
+int main()
+{
+	int count;
+	int tmp;
+	int total = 0;
+	for (count = 1; count <= 5; count++) {
+		printf("%d번째 정숫값 입력 후 enter!", count);
+		scanf("%d", &tmp);
+		total += tmp;
+	}
+	printf("읽은 정수 총합은 %d이다.", total);
+}
+*/

adda8730b4fe5d97fd5fb4ee9ce8ac2e036beb2b
ksy <ksy8230@hanmail.net>
2021-03-28 18:14:33 +0900

    선택 제어문 for - 주어진 문자열에서 특정 문자 개수 구하기

---
+++ 4-9.for4.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+/*
+int main()
+{
+	static char a[31] = "KOREA NATION OPEN UNIVERSITY";
+	int i, c;
+	c = 0; // 'O' 숫자 세는 카운터 변수
+	for (i = 0; i <= 29; i++) {
+		if (a[i] == 'O') {
+			c++;
+		}
+	}
+	printf("o의 개수= %d", c);
+}
+*/
